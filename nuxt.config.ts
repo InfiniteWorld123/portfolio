b79b1b83ad6dd9d44acc0a77a2e4e00ac0381c7e
‪Yaman Warda‬‏ <Wardayaman47@gmail.com>
2025-08-27 13:16:19 +0200

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -24,7 +24,7 @@ export default defineNuxtConfig({
     storageKey: 'nuxt-color-mode' // Key for storing the color mode
   },
   i18n: {
-    // langDir: 'myLocales',
+    langDir: 'i18n/locales',
     locales: [
       { code: 'en', name: 'English', file: 'en.json' },
       { code: 'ar', name: 'العربية', file: 'ar.json', dir: 'rtl' },
